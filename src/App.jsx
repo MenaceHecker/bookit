221ee2a02db6371e59f60361621664b2429e6d8d
LegionMammal978 <mattlloydhouse@gmail.com>
2023-10-01 17:51:50 -0400

    Add signup and activate pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
+import Activate from './components/Activate/Activate';
 import Homepage from '../src/components/Homepage/Homepage'
 import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 import TrailerPage from './components/TrailerPage/TrailerPage';
 const App = () => {
   return (
@@ -10,7 +12,9 @@ const App = () => {
         <Routes>
           <Route exact path='/' element={<Homepage/>} />
           <Route exact path='/' element={<TrailerPage/>}/>
+          <Route exact path="/activate" element={<Activate/>}/>
           <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/signup" element={<Signup/>}/>
         </Routes>
     </>
   );
---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Signup.css';
+
+function Signup() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+    setFormData({
+      email: '',
+      password: '',
+      password2: '',
+    });
+    navigate('/activate');
+  };
+
+  const onClose = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <div className="center_title">Sign Up</div>
+
+      <div className="signup_form">
+        <form onSubmit={handleSubmit} className="signup_container">
+          <div className="signup_form_row">
+            <label>
+              Email address:
+              <br/>
+              <input type="text" name="email" value={formData.email} onChange={handleInputChange}/>
+            </label>
+          </div>
+
+          <div className="signup_form_row">
+            <label>
+              Password:
+              <br/>
+              <input type="password" name="password" value={formData.password} onChange={handleInputChange}/>
+            </label>
+          </div>
+
+          <div className="signup_form_row">
+            <label>
+              Repeat password:
+              <br/>
+              <input type="password" name="password2" value={formData.password2} onChange={handleInputChange}/>
+            </label>
+          </div>
+
+          <div className="signup_button_row">
+            <button type="submit">Sign Up</button>
+            <button type="button" className="cancel_signup" onClick={onClose}>Back</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Signup;
