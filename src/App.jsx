61867815a46d6eb98708c6dbb7168001c5925c43
MenaceHecker <thedynamic2508@gmai.com>
2023-10-01 17:17:35 -0400

    HomePage integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Header from './components/header/header'
-import Home from './components/Home/Home';
-import Movies from './components/Movies/movies'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
+import Homepage from '../src/components/Homepage/Homepage'
+import TrailerPage from './components/TrailerPage/TrailerPage';
 const App = () => {
   return (
     <>
-    <Header/>
-    <Home/>
-    <Movies/>
+        <Routes>
+          <Route exact path='/' element={<Homepage/>} />
+          <Route exact path='/' element={<TrailerPage/>}/>
+        </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/TrailerPage/TrailerPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../header/header'
+const TrailerPage = () => {
+  return (
+    <div>
+        <Header/>
+        <h3>Now Playing</h3>
+    </div>
+  )
+}
+
+export default TrailerPage
