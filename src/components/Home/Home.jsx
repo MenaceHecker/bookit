029f1afbac994c4a288cdd0a1880da2f7c60ea79
LegionMammal978 <mattlloydhouse@gmail.com>
2023-10-03 13:50:43 -0400

    Link Book Now buttons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import video from '../../assets/trailer.mp4'
+import { useNavigate } from 'react-router-dom';
+import video from '../../assets/trailer.mp4';
 import './Home.css';
 
+
 function Home() {
+  const navigate = useNavigate();
+
   const openYoutubeVideo = () => {
     // Open the YouTube video in a new tab when the button is clicked
     window.open('https://www.youtube.com/watch?v=6ZfuNTqbHE8&t=1s&ab_channel=MarvelEntertainment', '_blank');
+  };
+
+  const bookNow = () => {
+    navigate('/BookingPage');
   };
 
   return (
@@ -13,7 +21,7 @@ function Home() {
       <div className='overlay'>
         <video src={video} autoPlay loop muted />
         <div className='containervid'>
-          <h3 className='title'>Avengers Infinity Wars</h3>
+          <h3 className='title' onClick={bookNow}>Avengers Infinity Wars</h3>
           <div className='rate'>
             <i className='fa fa-star'></i>
             <i className='fa fa-star'></i>
