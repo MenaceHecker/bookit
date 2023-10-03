0044ddf08b13b1b199d50ed199782176d4160ab5
LegionMammal978 <mattlloydhouse@gmail.com>
2023-10-03 13:24:46 -0400

    Add Coming Soon section

--- src/components/Movies/movies.jsx
+++ src/components/Movies/movies.jsx
@@ -12,28 +12,44 @@ const Movies = () => {
         .catch(error => console.error('Error:', error));
   }, []);
 
+  const displayMovie = ({ id, movieTrailerPicture, movieTitle, movieCast, movieCategory, movieTrailerVideo, movieSynopsis }) => (
+    <div key={id} className={'mov_cont'}>
+    <article className='movie__item'>
+      <div className='movie__item-image'>
+          <iframe width="480" height="275" src={movieTrailerVideo + '&autoplay=1&mute=1'} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      </div>
+      <h4 className='title'>{movieTitle}</h4>
+      <h3>Cast:<br></br>{movieCast}</h3>
+        <h3>Description:<br></br>{movieSynopsis}</h3>
+      <h3>Category:<br></br>{movieCategory}</h3>
+        <div id={'button_div'}>
+          <button>Book Now</button>
+        </div>
+    </article>
+    </div>
+  );
+
+  const showDatesCurrent = (dates) => {
+    const firstDate = dates.split(',')[0];
+    const startTime = Date.parse(firstDate + 'T00:00:00');
+    return isNaN(startTime) || Date.now() >= startTime;
+  };
+
   return (
-      <section id="now_showing">
+    <>
+      <section className="movie_section">
         <h5 className='title'>Now Showing</h5>
         <div className='container movie__container'>
-          {moviesData.map(({ id, movieTrailerPicture, movieTitle, movieCast, movieCategory, movieTrailerVideo, movieSynopsis }) => (
-              <div id={'mov_cont'}>
-              <article key={id} className='movie__item'>
-                <div className='movie__item-image'>
-                    <iframe width="480" height="275" src={movieTrailerVideo + '&autoplay=1&mute=1'} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                </div>
-                <h4 className='title'>{movieTitle}</h4>
-                <h3>Cast:<br></br>{movieCast}</h3>
-                  <h3>Description:<br></br>{movieSynopsis}</h3>
-                <h3>Category:<br></br>{movieCategory}</h3>
-                  <div id={'button_div'}>
-                    <button>Book Now</button>
-                  </div>
-              </article>
-              </div>
-          ))}
+          {moviesData.filter(({ movieShowDates }) => showDatesCurrent(movieShowDates)).map(displayMovie)}
         </div>
       </section>
+      <section className="movie_section">
+        <h5 className='title'>Coming Soon</h5>
+        <div className='container movie__container'>
+          {moviesData.filter(({ movieShowDates }) => !showDatesCurrent(movieShowDates)).map(displayMovie)}
+        </div>
+      </section>
+    </>
   );
 };
 
