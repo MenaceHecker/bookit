4fcbd37ba15b5a42578ef7317bb6381113cb739c
Alex Addington <axaddington@uga.edu>
2023-10-02 22:07:45 -0400

    api

--- src/components/Movies/movies.jsx
+++ src/components/Movies/movies.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './movies.css';
-
+import '../../logo.svg'
 const Movies = () => {
   const [moviesData, setMoviesData] = useState([]);
 
@@ -16,16 +16,17 @@ const Movies = () => {
       <section id="now_showing">
         <h5 className='title'>Now Showing</h5>
         <div className='container movie__container'>
-          {moviesData.map(({ id, poster, title, cast, disc, link }) => (
+          {moviesData.map(({ id, movieTrailerPicture, movieTitle, movieCast, movieCategory, movieTrailerVideo, movieSynopsis }) => (
               <article key={id} className='movie__item'>
                 <div className='movie__item-image'>
-                  <img className='movie_poster' src={poster} alt={title} />
+                  <img className='movie_poster' src={movieTrailerPicture} alt={movieTitle} />
                 </div>
-                <h4 className='title'>{title}</h4>
-                <h3>Cast: {cast}</h3>
-                <h3>{disc}</h3>
+                <h4 className='title'>{movieTitle}</h4>
+                <h3>Cast: {movieCast}</h3>
+                  <h3>{movieSynopsis}</h3>
+                <h3>{movieCategory}</h3>
                 <div className='movie__item-trailer'>
-                  <a href={link}>Watch Trailer</a>
+                  <a href={movieTrailerVideo}>Watch Trailer</a>
                 </div>
               </article>
           ))}
