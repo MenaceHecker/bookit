90760ac2b560cc18f6d2484900ff2c770b008ef5
Alex Addington <axaddington@uga.edu>
2023-10-03 00:12:00 -0400

    Implemented iframes and buttered the admin page

--- src/components/Movies/movies.jsx
+++ src/components/Movies/movies.jsx
@@ -17,15 +17,17 @@ const Movies = () => {
         <h5 className='title'>Now Showing</h5>
         <div className='container movie__container'>
           {moviesData.map(({ id, movieTrailerPicture, movieTitle, movieCast, movieCategory, movieTrailerVideo, movieSynopsis }) => (
+              <div id={'mov_cont'}>
               <article key={id} className='movie__item'>
                 <div className='movie__item-image'>
                     <iframe width="560" height="315" src={movieTrailerVideo} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </div>
                 <h4 className='title'>{movieTitle}</h4>
-                <h3>Cast: {movieCast}</h3>
-                  <h3>{movieSynopsis}</h3>
-                <h3>{movieCategory}</h3>
+                <h3>Cast:<br></br>{movieCast}</h3>
+                  <h3>Discription:<br></br>{movieSynopsis}</h3>
+                <h3>Category:<br></br>{movieCategory}</h3>
               </article>
+              </div>
           ))}
         </div>
       </section>
