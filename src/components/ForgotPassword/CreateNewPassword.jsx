781953f23f0e7c6252b9d3f777f1060dbf73c8bd
MenaceHecker <thedynamic2508@gmai.com>
2023-12-01 22:31:54 -0500

    Routing working

--- src/components/ForgotPassword/CreateNewPassword.jsx
+++ src/components/ForgotPassword/CreateNewPassword.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './CreateNewPassword.css';
 import Header from '../header/header';
