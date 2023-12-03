db7bede65995dcd8e738486c26fa88c6953faf31
LegionMammal978 <mattlloydhouse@gmail.com>
2023-12-03 17:18:57 -0500

    Revert "Add API object to deps"
    This reverts commit 460d3736d37d6965639a5fc6a598f10a5cea6550.

--- src/utils/API.js
+++ src/utils/API.js
@@ -263,13 +263,10 @@ class APITools {
 
 export function useApiData(callback, options = {}) {
   let api = useContext(APIContext);
-  let deps = options.deps ?? [];
-  if ('api' in options) {
+  if ('api' in options)
     api = options.api;
-    deps = [...deps, api];
-  }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const memoCallback = useCallback(callback, deps);
+  const memoCallback = useCallback(callback, options.deps ?? []);
   const [refreshToken, setRefreshToken] = useState(Symbol());
   useEffect(() => {
     const controller = new AbortController();
